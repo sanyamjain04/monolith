7cfd61b4ae9f1920ddd2925963f852909109d1f1
Shanu Khera <shanu.khera@nextroll.com>
2022-09-21 22:49:42 +0530

    before learning path page

---
+++ front-end/next-ui/components/footer.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+export const Footer = props => {
+  return (
+    <footer className="yellow">
+      <h4>
+          <a href="https://github.com/giganoto-com/monolith" target="_blank">
+            We ❤️  Open Source | Contribute to this source code - 
+            <Image src="/octocat.png" width="18" height="18" alt="Github logo" />
+          </a>
+      </h4>
+    </footer>
+  )
+}
--- front-end/next-ui/pages/index.js
+++ front-end/next-ui/pages/index.js
@@ -2,27 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import {Footer} from '../components/footer'
+
 export default function Home() {
   return (
-    <div>
+    <div className={styles.home}>
       <Head>
         <title>Giganoto - Code | Connect | Contribute</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <Image src="/main-header.svg" alt="Giganoto main header image" width="460" height="427" />
+        <Image src="/main-header.svg" alt="Giganoto main header image" width="304" height="282" />
         <h1>Giganoto</h1>
         <h2>Code 💻 | Connect 🔗 | Contribute 🚀</h2>
-        <h3>
-            <a href="https://github.com/giganoto-com/monolith">
-              Contribute to this source code 
-              <Image src="https://github.githubassets.com/images/icons/emoji/octocat.png" width="28" height="28" />
-            </a>
-        </h3>
         <hr />
       </header>
-      <main className={styles.main}>
+      <main>
         <button>
           <a href="#">
             {/* ToDo - https://github.com/giganoto-com/monolith/issues/1 */}
@@ -30,6 +26,7 @@ export default function Home() {
           </a>
         </button>
       </main>
+      <Footer />
     </div>
   )
 }
